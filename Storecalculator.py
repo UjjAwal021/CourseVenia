2f2f1ca0d5be2921ff3b1e57e37454f044fd29f3
unknown <gargujjawal20@gamil.com>
2022-04-15 01:59:00 +0530

    Add files

---
+++ Storecalculator.py
@@ -0,0 +1,14 @@
+
+sum = 0
+
+while(True):
+    number = input("Enter the price: \n")
+    if (userInput:='q'):
+        sum = sum + int(userInput)
+        print(f"order total so far: {sum}")
+
+
+    else:
+        print(f"Your Bill total is {sum}
+        print("Thanks for using")
+        break
